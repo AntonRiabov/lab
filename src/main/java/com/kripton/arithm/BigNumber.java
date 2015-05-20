7a4540c55fce58c535cfbc8e7c3cd3552887a51a
AntonRiabov <paladii@email.ua>
2015-05-20 04:29:52 +0300

    empty

--- src/main/java/com/kripton/arithm/BigNumber.java
+++ src/main/java/com/kripton/arithm/BigNumber.java
@@ -9,6 +9,7 @@ import java.text.DecimalFormat;
 public class BigNumber {
     public static final String FORMAT = "000000000";
     public static final int MAX_SIZE = Integer.MAX_VALUE;
+    public static final String PARSE_EXP = "(?<=\\G.........)";
     private int[] digits;
     public static int MOD = 1000000000;
 
@@ -76,7 +77,7 @@ public class BigNumber {
 
         for (int i = bStart; i <= bEnd; i++) {
             for (int j = aStart, shift = i + resultStart; j <= aEnd; j++) {
-                multiplyBuf = (long)a[j] * b[i];
+                multiplyBuf = (long) a[j] * b[i];
                 addBuf = multiplyBuf % MOD;
 
                 addBuf += result[shift + j];
@@ -98,14 +99,63 @@ public class BigNumber {
         return result;
     }
 
+    public BigNumber sub(BigNumber b) {
+        int[] bDigits = b.getDigits();
+        int[] result = null;
+        if (bDigits.length > digits.length) {
+            result = new int[bDigits.length];
+            sub(result, 0, bDigits, 0, bDigits.length, digits, 0, digits.length);
+        } else if (bDigits.length < digits.length) {
+            result = new int[digits.length];
+            sub(result, 0, digits, 0, digits.length, bDigits, 0, bDigits.length);
+        } else {
+            for (int i = digits.length - 1; i >= 0; i--) {
+                if (digits[i] > bDigits[i]) {
+                    result = new int[digits.length];
+                    sub(result, 0, digits, 0, digits.length, bDigits, 0, bDigits.length);
+                    break;
+                } else if (digits[i] < bDigits[i]) {
+                    result = new int[bDigits.length];
+                    sub(result, 0, bDigits, 0, bDigits.length, digits, 0, digits.length);
+                    break;
+                }
+                if (i == 0 && result == null) result = new int[2];
+            }
+        }
+        return new BigNumber(result);
+    }
 
 //    public int[] sub(int[] result, int resultStart, int[] a,  int[] b) {
 
 
     //    }
     public int[] sub(int[] result, int resultStart, int[] a, int aStart, int aEnd, int[] b, int bStart, int bEnd) {
+        for (int i = 0; i < bEnd - bStart; i++) {
+            int aPosition = aStart + i;
+            int bPosition = bStart + i;
+            if (b[bPosition] > a[aPosition] + result[resultStart + resultStart + i]) {
+                for (int k = aStart + i + 1; k < aEnd; k++) {
+                    if (a[k] != 0) {
+                        for (int l = k; l > i + aStart; l--) {
+                            --result[l - aStart + resultStart];
+                            result[l - aStart + resultStart - 1] += MOD;
+                        }
+                        break;
+                    }
+                }
+            }
+            result[resultStart + i] += a[aPosition] - b[bPosition];
+        }
+        return result;
+    }
 
-
+    public BigNumber karazuba(BigNumber b) {
+        int[] bDigits = b.getDigits();
+        int[] result = new int[bDigits.length + digits.length + 1];
+        int m = 0;
+        if (bDigits.length > digits.length) {
+            m = digits.length / 2;
+        }
 
         return null;
     }
@@ -120,16 +170,23 @@ public class BigNumber {
     }
 
     public static void main(String[] args) {
-        BigNumber bigNumber = new BigNumber(new int[]{123423, 2134231, 21341423, 2342134, 234, 2346345, 756865, 44567, 89});
-        BigNumber bigNumber2 = new BigNumber(new int[]{123423, 2134231, 21341423, 2342134, 234, 2346345, 756865, 44567, 89});
+//        BigNumber bigNumber = new BigNumber(new int[]{123423, 2134231, 21341423, 2342134, 234, 2346345, 756865, 445677, 89});
+//        BigNumber bigNumber2 = new BigNumber(new int[]{123423, 2134231, 21341423, 2342134, 234, 2346345, 756865, 445677, 89});
+        BigNumber bigNumber = new BigNumber("89000044567000756865002346345000000234002342134021341423002134231000123423");
+        BigNumber bigNumber2 = new BigNumber("89000044567000756865002346345000000234002342134021341423002134231000123423");
 
-        System.out.println(bigNumber.multiply(bigNumber));
-        long LONG_MASK = 0xffffffffL;
-        System.out.println(LONG_MASK);
+        System.out.println(bigNumber);
+        System.out.println(bigNumber2);
+
+        System.out.println(bigNumber.sub(bigNumber2));
+        System.out.println("-----------------------");
 //
 //        System.out.println(bigNumber);
-        BigInteger bigInteger = new BigInteger("89000044567000756865002346345000000234002342134021341423002134231000123423");
-        System.out.println(bigInteger.multiply(bigInteger));
+        BigInteger bigInteger = new BigInteger(new StringBuilder("89000044567000756865002346345000000234002342134021341423002134231000123423").reverse().toString());
+        BigInteger bigInteger2 = new BigInteger(new StringBuilder("89000044567000756865002346345000000234002342134021341423002134231000123423").reverse().toString());
+        System.out.println(bigInteger);
+        System.out.println(bigInteger2);
+        System.out.println(bigInteger2.subtract(bigInteger));
 //        System.out.println(bigNumber.add(bigNumber2));
 //        System.out.println(bigInteger.add(bigInteger).equals(new BigInteger(bigNumber.add(bigNumber2).toString())));
 //        int[] gg = new int[];
@@ -142,7 +199,7 @@ public class BigNumber {
         DecimalFormat myFormatter = new DecimalFormat(FORMAT);
         StringBuilder stringBuilder = new StringBuilder();
         int start = digits.length - 1;
-        while (digits[start] == 0) {
+        while (digits[start] == 0 && start > 0) {
             --start;
         }
         stringBuilder.append(digits[start]);
@@ -150,5 +207,15 @@ public class BigNumber {
             stringBuilder.append(myFormatter.format(digits[i]));
         }
         return stringBuilder.toString();
+    }
+
+    public BigNumber(String input) {
+        String[] arr = input.split("(?<=\\G.........)");
+        digits = new int[arr.length];
+//        StringBuilder builder = new StringBuilder();
+//        herf kbwj
+        for (int i = 0; i < arr.length; i++) {
+            digits[i] = Integer.parseInt(new StringBuilder(arr[i]).reverse().toString());
+        }
     }
 }
