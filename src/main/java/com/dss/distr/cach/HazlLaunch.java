2814270c2f02c6f53ef8c238d7488edab9309aa7
AntonRiabov <paladii@email.ua>
2015-05-25 08:15:30 +0300

    empty

--- src/main/java/com/dss/distr/cach/HazlLaunch.java
+++ src/main/java/com/dss/distr/cach/HazlLaunch.java
@@ -1,7 +1,14 @@
 package com.dss.distr.cach;
 
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
 /**
  * Created by paladii on 25.05.2015.
  */
 public class HazlLaunch {
+    public static void main(String[] args) {
+        HazelcastInstance hz1 = Hazelcast.newHazelcastInstance();
+        HazelcastInstance hz2 = Hazelcast.newHazelcastInstance();
+    }
 }
